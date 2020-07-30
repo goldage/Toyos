675a3fdc558c0ca5cc9d1c8de6e34973dc263b65
Laurence <thegreaterlee@gmail.com>
2020-07-30 23:57:56 +0800

    Learn how to enter 32-bit mode step by step.

---
+++ ex10_32bit_enter/32bit_main.asm
@@ -0,0 +1,28 @@
+[org 0x7c00] ; bootloader offset
+    mov bp, 0x9000  ; set the stack
+    mov sp, bp
+
+    mov bx, MSG_REAL_MODE
+    call print  ; This will be written after the BIOS message
+    
+    call switch_to_pm
+    jmp $       ; This will actually never be executed
+
+%include "../ex5_bootsector_functions/boot_sect_print.asm"
+%include "../ex9_32bit_gdt/32bit-gdt.asm"
+%include "../ex8_32bit_print/32bit-print.asm"
+%include "32bit_switch.asm"
+
+[bits 32]
+BEGIN_PM:   ; after the switch we will get here
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm    ; Note that this will be written at the top left corner
+    jmp $
+
+MSG_REAL_MODE db "Started in 16-bit real mode", 0
+MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
+
+; bootsector
+times 510 - ($-$$) db 0
+dw 0xaa55
+
